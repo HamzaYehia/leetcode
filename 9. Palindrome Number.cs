1bd7faee7ceca68f1d1c7c07c08e5221a33d75f5
Hamza Yehia <hamzayehiamakhlouf@gmail.com>
2022-04-09 00:02:24 +0200

    .

--- 9. Palindrome Number.cs
+++ 9. Palindrome Number.cs
@@ -12,17 +12,17 @@
 
 public class Solution {
     public bool IsPalindrome(int x) {
+        if (x < 0) return false;
+        
         int reverse = 0;
         int original = x;
 
         while (x > 0)
         {
-            // error handling
-            if (reverse > int.MaxValue) return false;
             reverse = (reverse * 10) + (x % 10);
             x /= 10;
         }
 
         return reverse == original;
-    }  
+    }
 }
