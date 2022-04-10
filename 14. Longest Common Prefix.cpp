b7cd16b385c3b9de1da010831c4ea7ca3eb431ad
Hamza Yehia <hamzayehiamakhlouf@gmail.com>
2022-04-11 00:36:53 +0200

    .

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,24 @@
+/**
+ * @problem name: Longest Common Prefix
+ * @problem link: https://leetcode.com/problems/longest-common-prefix/
+ *
+ * @solution time complexity: Linear O(n + m); n = outer loop, m = inner loop
+ * @solution status: accepted
+ *
+ * @author: Hamza Yehia (github.com/HamzaYehia)
+ * @date: 2022-Apr-10
+ */
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(strs[0]) != 0) {
+                strs[0] = strs[0].substr(0, strs[0].length() - 1);
+            }
+            if (strs[0] == "") break;
+        }
+        return strs[0];
+    }
+};
