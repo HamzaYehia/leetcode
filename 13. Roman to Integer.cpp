2737ae3fbb3674be0030845160f625bfcba4b5e0
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-08-03 23:26:33 +0200

    updated

--- 13. Roman to Integer.cpp
+++ 13. Roman to Integer.cpp
@@ -1,19 +1,20 @@
 /**
- * @problem name: 13. Roman to Integer
+ * @problem name: Roman to Integer
  * @problem link: https://leetcode.com/problems/roman-to-integer/
  *
- * @solution status: accepted
- * @solution time complexity: O(n) Where (n) is the length's of string (s)
+ * @time complexity: O(n)
+ * @space complexity: O(1)
  *
  * @author: Hamza Yehia (github.com/HamzaYehia)
  * @date: 2022-Apr-30
  */
 
+
 class Solution {
 public:
     int romanToInt(string s) {
 
-        map<char, int> RomanSymbols{
+        map<char, int> RomanSymbols {
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
 
         // (finalInt) initial value = last element in (s)
