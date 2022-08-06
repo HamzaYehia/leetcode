97d5b330d5c5970b4c7aa39602ad6712429ccd50
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-08-06 23:29:01 +0200

    O(n)

---
+++ 876. Middle of the Linked List.cpp
@@ -0,0 +1,29 @@
+/**
+ * @problem name: Middle of the Linked List
+ * @problem link: https://leetcode.com/problems/middle-of-the-linked-list/
+ *
+ * @time complexity: O(n)
+ * @space comlexity: O(1)
+ *
+ * @author: Hamza Yehia (github.com/HamzaYehia)
+ * @date: 2022-Aug-06
+ */
+
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == nullptr) return nullptr;
+
+        ListNode* slowRunner = head, * fastRunner = head;
+
+        while (fastRunner != nullptr && fastRunner->next != nullptr) {
+            if (fastRunner == nullptr) return slowRunner;
+
+            slowRunner = slowRunner->next;
+            fastRunner = fastRunner->next->next;
+        }
+
+        return slowRunner;
+    }
+};
