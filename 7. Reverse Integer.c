62053301c9cf4bfe5709cc306ef9f31bef9cf514
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-08-13 21:41:06 +0200

    time = O(log n)

--- 7. Reverse Integer.c
+++ 7. Reverse Integer.c
@@ -17,11 +17,8 @@ int reverse(int x) {
         int pop = x % 10;
         x /= 10;
 
-        if (reverse > INT_MAX / 10 ||
-            (reverse == INT_MAX / 10 && pop > 7)) return 0;
-
-        if (reverse < INT_MIN / 10 ||
-            (reverse == INT_MIN / 10 && pop < -8)) return 0;
+        if (reverse > INT_MAX / 10) return 0;
+        if (reverse < INT_MIN / 10) return 0;
 
         reverse = (reverse * 10) + pop;
     }
