e9d91219899e3472737c38bbe633d21d2db3dabe
Hamza Yehia <86762463+HamzaYehia@users.noreply.github.com>
2022-11-02 10:00:37 +0200

    Time: 147 ms (5.01%), Space: 39.6 MB (6.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if (ransomNote.length() > magazine.length()) return false;
         
         unordered_multiset<int> magazineChars;
 
